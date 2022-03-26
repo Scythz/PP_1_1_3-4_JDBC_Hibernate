b9cb4dd2c22cf4d8573f37924c5bd8358dbc80ac
Your Name <you@example.com>
2022-03-26 19:25:48 +0300

    done

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -6,33 +6,29 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    UserDaoJDBCImpl kek = new UserDaoJDBCImpl();
+
     public void createUsersTable() {
-        UserDaoJDBCImpl kek = new UserDaoJDBCImpl();
         kek.createUsersTable();
     }
 
     public void dropUsersTable() {
-        UserDaoJDBCImpl kek = new UserDaoJDBCImpl();
         kek.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        UserDaoJDBCImpl kek = new UserDaoJDBCImpl();
         kek.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        UserDaoJDBCImpl kek = new UserDaoJDBCImpl();
         kek.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        UserDaoJDBCImpl kek = new UserDaoJDBCImpl();
         return kek.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        UserDaoJDBCImpl kek = new UserDaoJDBCImpl();
         kek.cleanUsersTable();
     }
 }
