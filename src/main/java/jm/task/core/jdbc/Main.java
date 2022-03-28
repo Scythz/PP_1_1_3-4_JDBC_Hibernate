83c4efeb39f3093c8f3e1046b859617b957bdb33
Your Name <you@example.com>
2022-03-28 19:24:56 +0300

    done

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,26 +11,27 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
-
+        UserService us = new UserServiceImpl();
         User user1 = new User("Kek", "Kekovich", (byte) 1);
         User user2 = new User("Keke", "Kekovic", (byte) 2);
         User user3 = new User("Kekek", "Kekovi", (byte) 3);
         User user4 = new User("Kekeke", "Kekov", (byte) 4);
 
+        Consumer<User> saveUserAndSout = (x) -> {
+            us.saveUser(x.getName(), x.getLastName(), x.getAge());
+            System.out.println("User с именем – " + x.getName() + " добавлен в базу данных");
+        };
 
-        UserService us = new UserServiceImpl();
+
         us.createUsersTable();
-        us.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
-        System.out.println("User с именем – " + user1.getName() + " добавлен в базу данных");
-        us.saveUser(user1.getName(), user2.getLastName(), user2.getAge());
-        System.out.println("User с именем – " + user2.getName() + " добавлен в базу данных");
-        us.saveUser(user1.getName(), user3.getLastName(), user3.getAge());
-        System.out.println("User с именем – " + user3.getName() + " добавлен в базу данных");
-        us.saveUser(user1.getName(), user4.getLastName(), user4.getAge());
-        System.out.println("User с именем – " + user4.getName() + " добавлен в базу данных");
+        saveUserAndSout.accept(user1);
+        saveUserAndSout.accept(user2);
+        saveUserAndSout.accept(user3);
+        saveUserAndSout.accept(user4);
         List<User> list = us.getAllUsers();
         us.cleanUsersTable();
         us.dropUsersTable();
