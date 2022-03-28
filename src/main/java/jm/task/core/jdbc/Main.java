782870e517b95d58e5131fd5520accf12cde9f1a
Your Name <you@example.com>
2022-03-28 19:38:30 +0300

    done

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,38 +1,31 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
         UserService us = new UserServiceImpl();
-        User user1 = new User("Kek", "Kekovich", (byte) 1);
-        User user2 = new User("Keke", "Kekovic", (byte) 2);
-        User user3 = new User("Kekek", "Kekovi", (byte) 3);
-        User user4 = new User("Kekeke", "Kekov", (byte) 4);
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User("Kek", "Kekovich", (byte) 1));
+        users.add(new User("Keke", "Kekovic", (byte) 2));
+        users.add(new User("Kekek", "Kekovi", (byte) 3));
+        users.add(new User("Kekeke", "Kekov", (byte) 4));
 
         Consumer<User> saveUserAndSout = (x) -> {
             us.saveUser(x.getName(), x.getLastName(), x.getAge());
             System.out.println("User с именем – " + x.getName() + " добавлен в базу данных");
         };
 
-
         us.createUsersTable();
-        saveUserAndSout.accept(user1);
-        saveUserAndSout.accept(user2);
-        saveUserAndSout.accept(user3);
-        saveUserAndSout.accept(user4);
-        List<User> list = us.getAllUsers();
+        for (User user : users) {
+            saveUserAndSout.accept(user);
+        }
+        us.getAllUsers();
         us.cleanUsersTable();
         us.dropUsersTable();
 
